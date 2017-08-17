4223c2e3f48756c4e8d5951db2c9a3cd15a53b83
Luis Eduardo Jiménez Robles <eduardo90stablero@hotmail.com>
2017-08-17 00:46:33 -0700

    Python first attemps

---
+++ hello.py
@@ -0,0 +1,12 @@
+#variables
+miNombre = "Luis \nEduardo";
+miApellido = "Jimenez Robles";
+misAnos = 22;
+miPeso = 105.5;
+
+#prints
+print("Hola Mundo!");
+print(miNombre+" "+miApellido);
+print(type(miNombre));
+print(type(misAnos),misAnos);
+print(type(miPeso),miPeso);
