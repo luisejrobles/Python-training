82913b318685d0ec185cc774c4be71bfcf8910dd
Luis Eduardo Jiménez Robles <eduardo90stablero@hotmail.com>
2017-08-17 15:49:17 -0700

    Unidad 3
    Condicionales

---
+++ Unidad3/testing.py
@@ -0,0 +1,13 @@
+#Unidad 3 Python
+#Condicionales
+
+var1, var2 = 2, 1
+if var1 == var2:
+	print("Si lo es")
+else:
+	print("No lo es")
+
+if not var1 > var2:
+	print("No lo es")
+else:
+	print("Si lo es")
