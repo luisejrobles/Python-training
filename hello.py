b309a7c0ad782d10f94e79d4b0b05401a4646d6a
Luis Eduardo Jiménez Robles <eduardo90stablero@hotmail.com>
2017-08-17 09:44:32 -0700

    boolean, if, arithmetic

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ miPeso = 105.5
 todoJunto = "Hola mi nombre es {} tengo {} y peso {}"
 lista = (1, 2, 3, 4)
 miArreglo = [11, 22, 33, 44, 55]
+misPalabras = {'nombre': 'Luis', 'Apellido': 'Jimenez'}
+encendido = False
 
 #prints
 print("Hola Mundo!")
@@ -16,7 +18,6 @@ print(type(miPeso), miPeso)
 
 #format concatena variables a una cadena
 print(todoJunto.format(miNombre, miApellido, misAnos, miPeso))
-
 print("Imprimiendo lista:", lista)
 
 #agregando valor a mi arreglo
@@ -25,3 +26,16 @@ print("Imprimiendo mi arreglo con nuevo valor: ", miArreglo)
 
 #imprimiendo algo del arreglo
 print(miArreglo[0:2])
+print(misPalabras)
+
+#cambiando boolean
+if encendido:
+	print("hola")
+else:
+	print("no")
+encendido = True
+print(encendido)
+print(miArreglo[0] + miArreglo[1])
+
+#al cuadrado
+print(5 ** 2)
