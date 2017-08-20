d2189441c491de9ce7b9f0bc80202a2df267d4d9
Luis Eduardo Jiménez Robles <eduardo90stablero@hotmail.com>
2017-08-20 14:36:16 -0700

    new funct with lists
    remove, reverse, append, insert, extend

---
+++ Unidad5/test.py
@@ -0,0 +1,35 @@
+lista = [1,2,3,4,5,6,7,8,9,9]
+lista2 = [11,12,13]
+
+#print solo un numero
+print(lista[2])
+
+print("El tamano de la lista es: ",len(lista))
+
+
+#uno por uno
+for int in lista:
+	print(int)
+
+#imprimir algunos
+print(lista[1:4])
+
+#imprimir con salto
+print(lista[::2])
+
+#append agrega al final de la lista
+lista.append(12)
+#extend extiende la lista con otra lista
+lista.extend(lista2)
+#insert(posicion, item)
+lista.insert(2,11)
+#editando un valor de la lista
+lista[2] = 3
+#eliminando de la lista
+del lista[2]
+#eliminando de la lista buscando el valor
+lista.remove(9)
+#reverse invierte la cadena
+lista.reverse()
+
+print(lista)
