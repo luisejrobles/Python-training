fb8a617ec220f92c7c68081655b8ed378f24eef6
Luis Eduardo Jiménez Robles <eduardo90stablero@hotmail.com>
2017-08-20 14:41:11 -0700

    new functs
    reverse, sort, sorted + file for lecture final excercise

--- Unidad5/test.py
+++ Unidad5/test.py
@@ -19,17 +19,30 @@ print(lista[::2])
 
 #append agrega al final de la lista
 lista.append(12)
+
 #extend extiende la lista con otra lista
 lista.extend(lista2)
+
 #insert(posicion, item)
 lista.insert(2,11)
+
 #editando un valor de la lista
 lista[2] = 3
+
 #eliminando de la lista
 del lista[2]
+
 #eliminando de la lista buscando el valor
 lista.remove(9)
+
 #reverse invierte la cadena
 lista.reverse()
 
+#sort ordena lista
+lista.sort()
+lista.reverse()
+lista3 = sorted(lista)
 print(lista)
+print(lista3)
+
+
