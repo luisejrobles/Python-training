1df19332d3adb9f470825a1a310c0ecb11217346
Luis Eduardo Jiménez Robles <eduardo90stablero@hotmail.com>
2017-09-11 20:55:05 -0700

    input practice ready

--- Unidad6/Testing.py
+++ Unidad6/Testing.py
@@ -1,3 +1,12 @@
-#getting imputs
-input("Press any key to continue")
-print("TESTED!")
+#getting inputs
+#input("Press any key to continue")
+#print("TESTED!")
+
+#Getting inputs to variables
+#yourName = input("Escribe tu nombre\n")
+#print("Hola {}".format(yourName))
+
+#Operations with variables
+number = input('What is your favorite number?\n')
+number= int(number) + 5
+print("Your number +5 is: {}" .format(number))
