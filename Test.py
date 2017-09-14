2829ea7807b0a3f293545210bf32aa74e35ed03d
Luis Eduardo Jiménez Robles <eduardo90stablero@hotmail.com>
2017-09-14 09:45:19 -0700

    Testing objects

---
+++ Test.py
@@ -0,0 +1,18 @@
+#Classes and Objects
+class Person:
+	"""docstring for Person"""
+	def __init__(self,gender,name):
+		self.gender = gender 
+		self.name = name
+	def display(self):
+		print("You're a ", self.gender ,"and your name is: ",self.name)
+
+
+people = Person("Male","Luis Eduardo Jimenez Robles")
+people2 = Person("Female","Brenda Herrera")
+
+people.display()
+people2.display()
+
+ 
+		
